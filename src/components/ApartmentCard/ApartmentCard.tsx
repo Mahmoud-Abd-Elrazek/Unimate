90a7f03445043fdcbb8ab5da6085f00f7cf32bb6
Mahmoud Abdelrazek <mahmoudzanitty@gmail.com>
2025-04-13 17:52:40 +0200

    update apartment card

--- src/components/ApartmentCard/ApartmentCard.tsx
+++ src/components/ApartmentCard/ApartmentCard.tsx
@@ -4,6 +4,13 @@ import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { FaHeart } from "react-icons/fa";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
+import { IoLocationOutline } from "react-icons/io5";
+import { MdOutlineAccessTime } from "react-icons/md";
+import { BiBath } from "react-icons/bi";
+import { MdOutlineMeetingRoom } from "react-icons/md";
+import { LuBed } from "react-icons/lu";
+
+
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
@@ -45,59 +52,57 @@ const ApartmentCard = () => {
   };
 
   return (
-    <div className="">
+    <div className="bg-white border border-[#e0e0e0] rounded-xl overflow-hidden w-[420px] group relative transition-transform duration-300 ease-in-out hover:scale-[1.03] hover:shadow-md">
+      {/* Image Slider Section */}
+      <div className="relative">
+        <Swiper
+          ref={swiperRef}
+          modules={[Navigation, Pagination]}
+          pagination={{
+            clickable: true,
+            dynamicBullets: images.length >= 5,
+          }}
+          onSwiper={(swiper) => {
+            swiperRef.current = swiper;
+          }}
+          onSlideChange={(swiper) => setCurrentIndex(swiper.activeIndex)}
+          className="w-full h-[250px]"
+        >
+          {images.map((src, index) => (
+            <SwiperSlide key={index}>
+              <img
+                src={src}
+                alt="img"
+                className="w-full h-[250px] object-cover"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
 
-      <div className="bg-white rounded-xl shadow-lg overflow-hidden w-[310px] group relative ">
-        {/* Image Slider Section */}
-        <div className="relative">
-          <Swiper
-            ref={swiperRef}
-            modules={[Navigation, Pagination]}
-            pagination={{
-              clickable: true,
-              dynamicBullets: images.length >= 5,
-            }}
-            onSwiper={(swiper) => {
-              swiperRef.current = swiper;
-            }}
-            onSlideChange={(swiper) => setCurrentIndex(swiper.activeIndex)}
-            className="w-full h-[250px]"
+        {/* Custom Navigation Buttons */}
+        {currentIndex > 0 && (
+          <button
+            ref={prevRef}
+            onClick={handlePrevClick}
+            className="absolute left-3 top-1/2 -translate-y-1/2 bg-white w-[32px] h-[32px] rounded-full shadow-lg z-10 flex items-center justify-center cursor-pointer  group-hover:opacity-100 transition-opacity duration-300"
           >
-            {images.map((src, index) => (
-              <SwiperSlide key={index}>
-                <img
-                  src={src}
-                  alt="img"
-                  className="w-full h-[250px] object-cover"
-                />
-              </SwiperSlide>
-            ))}
-          </Swiper>
+            <IoIosArrowBack className="text-[#000]" style={{ width: "100%", height: "20px" }} />
+          </button>
+        )}
 
-          {/* Custom Navigation Buttons */}
-          {currentIndex > 0 && (
-            <button
-              ref={prevRef}
-              onClick={handlePrevClick}
-              className="absolute left-3 top-1/2 -translate-y-1/2 bg-white w-[32px] h-[32px] rounded-full shadow-lg z-10 flex items-center justify-center cursor-pointer  group-hover:opacity-100 transition-opacity duration-300"
-            >
-              <IoIosArrowBack className="text-[#000]" style={{ width: "100%", height: "20px" }} />
-            </button>
-          )}
-
-          {currentIndex < images.length - 1 && (
-            <button
-              ref={nextRef}
-              onClick={handleNextClick}
-              className="absolute right-3 top-1/2 -translate-y-1/2 bg-white w-[32px] h-[32px] rounded-full shadow-lg z-10 flex items-center justify-center cursor-pointer  group-hover:opacity-100 transition-opacity duration-300"
-            >
-              <IoIosArrowForward className="text-[#000]" style={{ width: "100%", height: "20px" }} />
-            </button>
-          )}
-          {/* Custom styles for pagination dots */}
-          <style
-            dangerouslySetInnerHTML={{
-              __html: `
+        {currentIndex < images.length - 1 && (
+          <button
+            ref={nextRef}
+            onClick={handleNextClick}
+            className="absolute right-3 top-1/2 -translate-y-1/2 bg-white w-[32px] h-[32px] rounded-full shadow-lg z-10 flex items-center justify-center cursor-pointer  group-hover:opacity-100 transition-opacity duration-300"
+          >
+            <IoIosArrowForward className="text-[#000]" style={{ width: "100%", height: "20px" }} />
+          </button>
+        )}
+        {/* Custom styles for pagination dots */}
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
                 .swiper-pagination-bullet {
                   background-color: white !important; /* White background */
                   opacity: .5 !important; /* Ensure visibility */
@@ -109,48 +114,93 @@ const ApartmentCard = () => {
                   opacity: 1 !important; /* Ensure visibility */
                 }
               `,
-            }}
-          />
+          }}
+        />
 
-          {/* Favorite Icon on the Left */}
-          <div title="Add to Favourites" className="absolute top-3 left-3 bg-white p-2 rounded-full shadow-md cursor-pointer z-10">
-            <FaHeart className="text-[#00000080] hover:text-red-500 hover:scale-110 transition duration-300" />
+        {/* Favorite Icon on the Left */}
+        <div title="Add to Favourites" className="absolute top-3 left-3 bg-white p-2 rounded-full shadow-md cursor-pointer z-10">
+          <FaHeart className="text-[#00000080] hover:text-red-500 hover:scale-110 transition duration-300" />
+        </div>
+      </div>
+
+      {/* Details Section */}
+      <div className="p-3 text-right">
+        <div className="flex items-center justify-between mb-2">
+          <span
+            className="block text-[14px] font-bold text-[#2002C6] text-right leading-[20px]">
+            6,600/mo
+          </span>
+          <h3 className="text-[16px] font-semibold text-[#212529]">
+            الشؤون · أولاد · 3 غرف · 6 ضيف · الدور الثالث
+          </h3>
+        </div>
+
+        {/* location */}
+        <div className="flex items-center justify-end mb-2">
+          <span className="text-[14px] text-[#515151] mr-2"> الشؤون / شارع ابو علاء </span>
+          <IoLocationOutline className="text-[#515151]" />
+        </div>
+
+        {/* descritpion */}
+        <p className="text-[14px] text-[#515151]">
+          سكن مناسب للطلبه و العائلات · قريب من المواصلات العامة · قريب من المحلات التجارية · قريب من المطاعم · قريب من الجامعه
+        </p>
+
+        {/* Services */}
+        <div className="flex items-center justify-end mb-2 mt-2 gap-x-2 pb-2 border-b border-[#e0e0e0]">
+          {/* bed */}
+          <div className="flex items-center justify-end mb-2 mt-2">
+            <span className="text-[14px] text-[#111111] mr-2"> 12 سرير </span>
+            <LuBed className="text-[#111111]" />
+          </div>
+          {/* room */}
+          <div className="flex items-center justify-end mb-2 mt-2">
+            <span className="text-[14px] text-[#111111] mr-2">4 غرفه</span>
+            <MdOutlineMeetingRoom className="text-[#111111]" />
+          </div>
+          {/* bath */}
+          <div className="flex items-center justify-end mb-2 mt-2">
+            <span className="text-[14px] text-[#111111] mr-2">1 حمام</span>
+            <BiBath className="text-[#111111]" />
           </div>
         </div>
 
-        {/* Details Section */}
-        <div className="p-4 text-right">
-          <h3 className="text-[17px] font-semibold text-[#212529]">
-            الشؤون · أولاد · 3 غرف · 6 ضيف · الدور الثالث
-          </h3>
-          <p className="text-base text-[#6C757D]">ج.م 600 في الشهر</p>
-          <p className="text-[12px] text-[#6C757D] text-sm">قبل يومين</p>
+        {/* Some Services */}
+        <div className="flex items-center justify-end gap-x-2">
+          <span className="px-3 py-1 bg-[#F1F5F9] text-sm text-[#111111] rounded-full">
+            +10 اخري
+          </span>
+          <span className="px-3 py-1 bg-[#F1F5F9] text-sm text-[#111111] rounded-full">
+            WiFi
+          </span>
+          <span className="px-3 py-1 bg-[#F1F5F9] text-sm text-[#111111] rounded-full">
+            غاز طبيعي
+          </span>
+          <span className="px-3 py-1 bg-[#F1F5F9] text-sm text-[#111111] rounded-full">
+            مكيف
+          </span>
         </div>
 
-        {/* Owner Section - Right to Left */}
-        <div className="cursor-pointer hover:bg-[#F1F3F4] border-t px-4 py-3 flex flex-row-reverse items-center">
-          <img
-            src="https://scontent.fcai20-2.fna.fbcdn.net/v/t39.30808-1/455031373_1934517026975496_2310134617658645041_n.jpg?stp=cp6_dst-jpg_s200x200_tt6&_nc_cat=105&ccb=1-7&_nc_sid=1d2534&_nc_ohc=Yi0xgVUN9KcQ7kNvgE8BX6x&_nc_oc=AdjxDwGv0WMWMla9tsq7UIDVmTlOATLneMT8MCVoYuySCUk5RRxepTsVQ9xgRLAHHdg&_nc_zt=24&_nc_ht=scontent.fcai20-2.fna&_nc_gid=tkJXZfGJiMJmS-GISUpCnA&oh=00_AYGlMRoN-aKQfHGJXBPvVurcIzfI1eZ6qizHGYdDOTo61w&oe=67DBB602"
-            alt="Owner"
-            className="-[50px] h-[50px] rounded-full ml-3"
-          />
-          <div className="text-right">
-            <p className="text-lg font-semibold text-[#212529]">عمرو محمد عبده</p>
-            <div className="text-gray-500 text-sm">
-              <p className="text-[#6C757D] font-semibold">صاحب عقار متميز</p>
-              <div className="text-gray-500 text-sm flex items-center justify-end gap-0">
-                <MdOutlineStar className="text-[15px] text-yellow-500" />
-                <MdOutlineStar className="text-[15px] text-yellow-500" />
-                <MdOutlineStar className="text-[15px] text-yellow-500" />
-                <MdOutlineStarBorder className="text-[15px] text-yellow-500" />
-                <MdOutlineStarBorder className="text-[15px] text-yellow-500" />
-              </div>
+        <div className="flex items-center justify-between">
+          {/* deuration */}
+          <div className="flex items-center justify-end mb-2 mt-2">
+            <span className="text-[13px] text-[#515151] mr-2">قبل يوم </span>
+            <MdOutlineAccessTime className="text-[#515151]" />
+          </div>
+
+          {/* number of comments & rating */}
+          <div className="flex items-center justify-end mb-2 mt-2">
+            <span className="text-[14px] text-[#515151] mr-2"> +10 تعليقات </span>
+
+            <div className="flex items-center justify-end mb-2 mt-2">
+              <span className="text-[13px] text-[#515151] mr-2">4.5 </span>
+              <MdOutlineStar className="text-[#515151]" />
             </div>
           </div>
         </div>
       </div>
-    </div>
 
+    </div>
   );
 };
 
