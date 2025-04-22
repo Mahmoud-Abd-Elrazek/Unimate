5b71afb4faeb9ffd64ffb96fdf91b5c5dd4293ff
Reem Mohamed <ream2004523farouk@gmail.com>
2025-04-22 19:14:06 +0200

    create the step2 page and change the theme of the stepper

--- src/components/StepperNav/StepperNav.tsx
+++ src/components/StepperNav/StepperNav.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Stepper, Step, StepLabel, Box } from '@mui/material';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-const steps = ['صور المسكن','معلومات المسكن','خدمات المسكن','غرف المسكن'];
+const steps = ['صور المسكن', 'معلومات المسكن', 'خدمات المسكن', 'غرف المسكن'];
 steps.reverse();
-const paths = ['/createpost/step1', '/createpost/step2', '/createpost/step3','/createpost/step4'];
+const paths = ['/createpost/step1', '/createpost/step2', '/createpost/step3', '/createpost/step4'];
 
 const StepperNav: React.FC = () => {
   const location = useLocation();
@@ -12,8 +12,22 @@ const StepperNav: React.FC = () => {
   const activeStep = paths.indexOf(location.pathname);
 
   return (
+
     <Box sx={{ width: '100%', direction: 'rtl' }}>
-      <Stepper activeStep={activeStep}>
+
+      <Stepper activeStep={activeStep}
+        sx={{
+          '& .MuiStepIcon-root': {
+            color: 'gray', // default (future steps)
+          },
+          '& .Mui-completed .MuiStepIcon-root': {
+            color: 'red', // completed
+          },
+          '& .Mui-active .MuiStepIcon-root': {
+            color: 'red', // active
+          }
+        }}
+      >
         {[...steps].reverse().map((label, index) => {
           const stepIndex = steps.length - 1 - index; // reverse index
           return (
@@ -23,6 +37,7 @@ const StepperNav: React.FC = () => {
           );
         })}
       </Stepper>
+
     </Box>
   );
 };
--- src/pages/CreatePost/createPost.tsx
+++ src/pages/CreatePost/createPost.tsx
@@ -7,8 +7,8 @@ import { PiStudentFill } from 'react-icons/pi';
 
 const CreatePost: React.FC = () => {
   return (
-    <div className=' min-h-screen mt-1 mr-3' dir='rtl'> 
-      <h1 className='text-3xl font-semibold text-right flex'>استديو اصحاب العقارات <span><PiStudentFill className='w-10 h-10 MainColorText'/></span></h1>
+    <div className=' min-h-screen px-10' dir='rtl'> 
+      <h1 className='text-3xl font-semibold text-center flex mr-5 justify-center items-center'>استديو اصحاب العقارات <span><PiStudentFill className='w-10 h-10 MainColorText'/></span></h1>
     <Box sx={{ p: 4 }} className="">
       <div className='mx-24 '>
       <StepperNav />
