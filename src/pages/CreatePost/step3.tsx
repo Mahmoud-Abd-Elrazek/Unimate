1ff3527d4d017977cca0a7ce65d7e4bfe246501e
Reem Mohamed <ream2004523farouk@gmail.com>
2025-05-02 17:54:58 +0300

    Resolved merge conflict in step 3 during git pull

--- src/pages/CreatePost/step3.tsx
+++ src/pages/CreatePost/step3.tsx
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
-import { MdHelpOutline } from 'react-icons/md';
-import { Link, useNavigate } from 'react-router-dom';
-// import Housing_services_Side_Bar from '../../components/Housing_services_Side_Bar/housing_services_Side_Bar';
-=======
 import { useNavigate } from "react-router-dom";
 import { FaCheckCircle, FaPlusCircle } from "react-icons/fa";
-import { MdKitchen, MdMicrowave, MdBlender, MdLocalDrink } from "react-icons/md";
+import {  MdMicrowave, MdBlender, MdLocalDrink } from "react-icons/md";
 import { GiGasStove } from "react-icons/gi";
 import { TbFridge } from "react-icons/tb";
 import { PiCookingPotBold } from "react-icons/pi";
 import { LuWashingMachine } from "react-icons/lu";
->>>>>>> f7c57efd8e3c58afd29cd8802ee010cae1cbbce2
 
 const kitchenServices = [
   { label: "مطبخ", icon: <PiCookingPotBold size={20} />, available: false },
@@ -36,67 +30,6 @@ const StepThree = () => {
   const navigate = useNavigate();
 
   return (
-<<<<<<< HEAD
-    <div style={{ padding: '2rem' }}>
-      {/* the sidebar */}
-      <div className=" flex justify-end " dir='rtl'>
-        {/* side bar */}
-        <aside className=" min-h-full bg-[#f4f4f4] w-[250px]">
-        <div className=" w-full h-full" style={{ padding: "10px" }}>
-            {/* <h3>Sideba r</h3> */}
-            <ul className="grid gap-3 mt-1 ">
-                <li className="">
-                    الحمام
-                </li>
-                <li className="">
-                    الحمام
-                </li>
-                <li className="">
-                    الحمام
-                </li>
-                <li className="">
-                    الحمام
-                </li>
-                <li className="">
-                    الحمام
-                </li>
-               
-
-              
-                
-            </ul>
-        </div>
-        </aside>
-        {/* main content */}
-        <main className="content w-full h-full">
-          heloo world
-          <p>Welcome to the step three of the post creation process.</p>
-        </main>
-
-      </div>
-
-      {/* buttons */}
-      <hr className="border-t border-gray-400 w-5/6 mx-auto my-8" />
-
-      <div className=' flex justify-between items-center mt-5'>
-        <div>
-          <Link to='/auther/help' className="flex justify-end">
-            <span className="">
-              <MdHelpOutline className="IconSize" />
-            </span>
-            المساعده
-          </Link>
-        </div>
-        <div className='flex justify-between items-center w-[15rem] '>
-          <button onClick={() => navigate('/createpost/step2')} className='bg-black text-white w-20 h-10 rounded-md'>
-            رجوع
-          </button>
-          <button onClick={() => navigate('/createpost/step4')} className='MainColorBG text-white w-20 h-10 rounded-md'>
-            التالى
-          </button>
-        </div>
-      </div>
-=======
     <div className="flex flex-col min-h-screen  p-6">
       {/* المحتوى الرئيسي */}
       <div className="flex flex-col md:flex-row gap-6 flex-1">
@@ -151,7 +84,6 @@ const StepThree = () => {
           التالي
         </button>
       </div>
->>>>>>> f7c57efd8e3c58afd29cd8802ee010cae1cbbce2
     </div>
   );
 };
