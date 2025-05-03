07d8a4b5c43df05dff09336cc1f9b750598ba00e
Reem Mohamed <ream2004523farouk@gmail.com>
2025-05-03 17:43:12 +0300

    create the pages of edit the profile info

---
+++ src/pages/EditProfilePage/Edit_contact_information/edit_contact_information.tsx
@@ -0,0 +1,8 @@
+
+export default function Edit_contact_information() {
+  return (
+    <div>
+      this is the second page
+    </div>
+  )
+}
---
+++ src/pages/EditProfilePage/Edit_personal_information/edit_profile_information.tsx
@@ -0,0 +1,8 @@
+
+export default function edit_profile_information() {
+  return (
+    <div>
+      this is the first page
+    </div>
+  )
+}
