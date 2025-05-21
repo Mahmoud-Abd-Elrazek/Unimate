9e97840914c661a052d82aec53bcf48817232a8c
Reem Mohamed <ream2004523farouk@gmail.com>
2025-05-21 21:37:19 +0300

    want to try the project in vercel

--- src/components/ApartmentCard/ApartmentCard.tsx
+++ src/components/ApartmentCard/ApartmentCard.tsx
@@ -184,14 +184,9 @@ const ApartmentCard = ({ className = "", edit = false, data, id }: ApartmentCard
           }}
         />
 
-<<<<<<< HEAD
-        <div onClick={() => AddtoFav(String(id ?? ""))} title="Add to Favourites" className="absolute top-3 left-3 p-2 rounded-full shadow-md cursor-pointer z-10 bg-[#f8fafc]">
-          
-=======
+
 
         <div onClick={() => AddtoFav(String(id ?? ""))} title="Add to Favourites" className="absolute top-3 left-3 p-2 rounded-full shadow-md cursor-pointer z-10 bg-[#f8fafc]">
-
->>>>>>> f03085dda033b111a8934fa99bb3f5a32a0b22a6
           <FaHeart className="text-[#00000080] hover:text-red-500 hover:scale-110 transition duration-300" />
         </div>
       </div>
