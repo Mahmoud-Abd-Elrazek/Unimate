642987543860a5c60e492d4867a35d140b42f09b
Reem Mohamed <ream2004523farouk@gmail.com>
2025-05-28 00:43:52 +0300

    merge the conf

--- src/components/SearchBar/SearchBarWithFilters.tsx
+++ src/components/SearchBar/SearchBarWithFilters.tsx
@@ -5,18 +5,11 @@ import SearchBar from './UI/SearchBar'
 const SearchBarWithFilters = () => {
   return (
     <div className="w-full flex flex-row-reverse justify-center gap-2 h-[40px] max-w-[1050px] dark:bg-secondary_BGD">
+
       <SearchBar />
-<<<<<<< HEAD
-      <Button variant="outline" className="flex gap-2 h-full">
+      <Button variant="outline" className="flex gap-2 h-full bg-[#ef4444] text-white">
         <SlidersHorizontal />
         <span className="hidden sm:inline">فلتر نتائجك</span>
-=======
-
-      {/* Filter Button */}
-      <Button variant="outline" className="flex gap-2 h-full bg-[#ef4444] text-white">
-        <SlidersHorizontal className="" />
-        <span className="hidden sm:inline"> ابحث</span>
->>>>>>> d1e77b7308e52e82df293ed00568a045f6725141
       </Button>
     </div>
   );
